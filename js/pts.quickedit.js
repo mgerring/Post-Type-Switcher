dc582abaaa613588a8c7b03697400acb6ce4e630
Matthew Gerring <gerring.matthew@gmail.com>
2012-10-09 17:27:07 -0700

    added quick edit and bulk edit functionality

---
+++ js/pts.quickedit.js
@@ -0,0 +1,29 @@
+function quickEditPT() {
+	var $ = jQuery;
+	var _edit = inlineEditPost.edit;
+	inlineEditPost.edit = function(id) {
+		var args = [].slice.call(arguments);
+		_edit.apply(this, args);
+
+		if (typeof(id) == 'object') {
+			id = this.getId(id);
+		}
+		var
+		// editRow is the quick-edit row, containing the inputs that need to be updated
+		editRow = $('#edit-'+id),
+		// postRow is the row shown when a book isn't being edited, which also holds the existing values.
+		postRow = $('#post-'+id),
+		
+		// get the existing values
+		post_type = $('.post_type', postRow).data('post-type');
+		
+		// set the values in the quick-editor
+		$('select[name="pts_post_type"] option[value="'+post_type+'"]', editRow).attr('selected','selected');
+	};
+}
+// Another way of ensuring inlineEditPost.edit isn't patched until it's defined
+if (inlineEditPost) {
+	quickEditPT();
+} else {
+	jQuery(quickEditPT);
+}
